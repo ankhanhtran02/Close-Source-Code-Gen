ffd7812f24a5096111e4b49d100b2ae96b92dd4e
Khanh Tran <ankhanhtran02@gmail.com>
2025-08-01 00:58:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Close Source Code Generation
+
+## Overview
+
+This repository provides a script for generating code completions using the Gemini API and a HuggingFace dataset. It is designed to automate the process of sending prompts to the Gemini model and saving the generated responses for further analysis or evaluation. The script supports configurable parameters such as model selection, temperature, top-p, top-k, candidate count, and output token limits.
+
+## Instructions
+
+### 1. Install Dependencies
+Run the following commands in your termina
+```sh
+conda create -n close-source python=3.11
+conda activate close-source
+pip install -r requirements.txt
+```
+### 2. Generate code completions
+Get your [Gemini API key](https://aistudio.google.com/apikey) then run the following command in your terminal:
+
+```sh
+python3 generate.py \
+  --api_key YOUR_API_KEY \
+  --data AnhMinhLe/repoexec_comparison_dataset \
+  --split repoexec_bm25_final \
+  --save_path outputs/repoexec_bm25_final_generated.jsonl \
+```
+
+View `generate.py` module for descriptions of arguments.
+
---
+++ generate.py
@@ -0,0 +1,65 @@
+from google import genai
+from google.genai import types
+import argparse
+from datasets import load_dataset
+import json
+import os
+from tqdm import tqdm
+
+SYSTEM_PROMPT = '''You are a helpful coding assistant.''' 
+
+def generate():
+  parser = argparse.ArgumentParser(description="Arguments to generate code completion")
+  parser.add_argument("--api_key", type=str, required=True, help="API key of Gemini API")
+  parser.add_argument("--model", type=str, default="gemini-2.5-pro", help="Gemini model name")
+  parser.add_argument("--data", type=str, default="AnhMinhLe/repoexec_comparison_dataset", help="HuggingFace dataset ID")
+  parser.add_argument("--split", type=str, default="repoexec_bm25_final", help="HuggingFace dataset split")
+  parser.add_argument("--save_path", type=str, default="outputs/repoexec_bm25_final_generated.jsonl")
+  parser.add_argument("--temperature", type=float, default=1.0, help="Controls the randomness of the output. Range:[0.0, 2.0]")
+  parser.add_argument("--top_p", type=float, default=1.0, help="The maximum cumulative probability of tokens to consider when sampling")
+  parser.add_argument("--top_k", type=int, default=40, help="The maximum number of tokens to consider when sampling")
+  parser.add_argument("--candidate_count", type=int, default=1, help="Number of generated responses to return")
+  parser.add_argument("--max_output_tokens", type=int, default=2048, help="The maximum number of tokens to include in a response candidate")
+
+  args = parser.parse_args()
+  
+  ds = load_dataset(args.data, split=args.split)
+  samples = []
+  client = genai.Client(api_key=args.api_key)
+  for sample in tqdm(ds): 
+    response = client.models.generate_content(
+        model=args.model,
+        config=types.GenerateContentConfig(
+          system_instruction=SYSTEM_PROMPT,
+          temperature=args.temperature,
+          top_p=args.top_p,
+          top_k=args.top_k,
+          candidate_count=args.candidate_count,
+          max_output_tokens=args.max_output_tokens,
+        ),
+        contents=sample['prompt']
+    )
+
+    response_texts = []
+    if args.candidate_count == 1:
+      response_texts.append(response.text)
+    else:
+      for candidate in response.candidates:
+        response_texts.append(candidate.content.parts[0].text)
+
+    line =  {
+        "task_id":sample["id"],
+        "prompt":sample["prompt"],
+        "response":response_texts
+      }
+    samples.append(line)
+  
+  os.makedirs(os.path.dirname(args.save_path), exist_ok=True)
+
+  with open(args.save_path, "w", encoding="utf-8") as f:
+    for sample in samples:
+      f.write(json.dumps(sample) + "\n")
+    print(f"Saved responses to {args.save_path}")
+
+if __name__ == "__main__":
+  generate()
